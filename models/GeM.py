e54542c6076d6a4510959422ad08ec677382b882
hidayet ergin <57322970+hidayetergn@users.noreply.github.com>
2025-12-21 10:20:35 +0300

    Add files via upload

---
+++ models/GeM.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GeM(nn.Module):
+    """
+    Generalized Mean Pooling
+    """
+
+    def __init__(self, p=3.0, eps=1e-6):
+        super().__init__()
+        self.p = nn.Parameter(torch.ones(1) * p)
+        self.eps = eps
+
+    def forward(self, x):
+        return F.avg_pool2d(
+            x.clamp(min=self.eps).pow(self.p),
+            (x.size(-2), x.size(-1))
+        ).pow(1.0 / self.p)
---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .gem import GeM
+from .convnext_part_model import ConvNeXtPartModel
+
+__all__ = [
+    "GeM",
+    "ConvNeXtPartModel",
+]
